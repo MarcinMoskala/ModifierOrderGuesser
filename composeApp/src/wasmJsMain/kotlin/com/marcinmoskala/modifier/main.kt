157255d41918c2d30f5b8e7444aa142feef55379
Marcin Moskała <marcinmoskala@gmail.com>
2024-12-06 22:35:29 +0100

    Add info for iOS users

--- composeApp/src/wasmJsMain/kotlin/com/marcinmoskala/modifier/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/marcinmoskala/modifier/main.kt
@@ -3,10 +3,12 @@ package com.marcinmoskala.modifier
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.w3c.dom.asList
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     document.getElementById("no_wasm_comment")?.remove()
+    document.body?.children?.asList()?.forEach { it.remove() }
     ComposeViewport(document.body!!) {
         App()
     }
