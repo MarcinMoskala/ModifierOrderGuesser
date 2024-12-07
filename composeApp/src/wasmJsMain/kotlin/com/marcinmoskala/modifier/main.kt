c3c5a1fb0a6d3033def401ea4d1470fa0d6cd99b
Marcin Moskała <marcinmoskala@gmail.com>
2024-12-07 15:48:40 +0100

    Add Spinner

--- composeApp/src/wasmJsMain/kotlin/com/marcinmoskala/modifier/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/marcinmoskala/modifier/main.kt
@@ -8,6 +8,7 @@ import org.w3c.dom.asList
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     document.getElementById("no_wasm_comment")?.remove()
+    document.getElementById("spinner")?.remove()
     document.body?.children?.asList()?.forEach { it.remove() }
     ComposeViewport(document.body!!) {
         App()
